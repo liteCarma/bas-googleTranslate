41fe0f62737866427101c80fc4f1cdce4a29cf35
Denis <litecarma@gmail.com>
2022-04-09 14:31:26 +0700

    chore(): updating technical data for the got dependency

--- gtranslate_code.js
+++ gtranslate_code.js
@@ -1,3 +1,3 @@
- /*Dat:eyJzIjoiZ290In0*/
+/*Dat:eyJzIjoiZ290R1VJR2V0In0*/
 _call_function(gtranslate,{ "from": (<%= from %>),"text": (<%= text %>),"to": (<%= to %>) })!
 <%= variable %> = _result_function()
